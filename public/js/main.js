64698d0d434ea88699a89e61da19fd35eca2740c
Dr. Amatón <ing.amaton@gmail.com>
2020-04-10 18:24:04 -0500

    interfaz de usuario lista

--- public/js/main.js
+++ public/js/main.js
@@ -3,34 +3,102 @@
  *
  * Archivo principal para todas las funciones globales de JavaScript
  */
+let map;
+let markers = [];
+let infoWindow;
+let locationSelect;
 
 /**
- * Enviar una peticion al servidor para que guarde la informacion del lugar
- * En esta funcion estaremos usando JQuery
- *
- * TODO: implementar Axios (y webpack)
+ * Centrar el mapa en el Tec Laguna, definir el uso del boton de buscar
  */
-function savePlace() {
-	const payload = { gatos: 42 };	/* Esta es nuestra informacion en el formato correcto */
+function initMap() {
+    var itl = {
+        lat: 25.535365,
+        lng: -103.435297
+    };
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: itl,
+        zoom: 11,
+        mapTypeId: 'roadmap',
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+        }
+    });
+    infoWindow = new google.maps.InfoWindow();
 
-	$.ajax({
-		type: "POST",
-		url: "api/places",
-		data: payload,
-		dataType: "json",
+    searchButton = document.getElementById("searchButton").onclick = searchLocations;
 
-		success: (response) => {
-			/* El json seria algo como { "result": 42 } */
-			console.log(response.result);
-		},
-		error: (response) => {
-			/* Hay que revisar el mensaje que nos dio la respuesta */
-			console.log("el servidor respondio con un error");
-		},
-
-	});
+    locationSelect = document.getElementById("locationSelect");
+    locationSelect.onchange = function () {
+        var markerNum = locationSelect.options[locationSelect.selectedIndex].value;
+        if (markerNum != "none") {
+            google.maps.event.trigger(markers[markerNum], 'click');
+        }
+    };
 }
 
-$(document).ready(() => {
-	savePlace();
-});
+/**
+ * Validar los parametros e iniciar la consulta
+ */
+function searchLocations() {
+    const keyword = document.getElementById("addressInput").value;
+    const radius = document.getElementById("radiusSelect").value;
+    const type = document.getElementById("typeInput").value;
+
+    if (radius > 50 || radius < 10) {
+        alert("El radio especificado es demasiado grande o demasiado pequeño");
+        return;
+    }
+
+    if (!keyword) {
+        alert("No especifico ninguna palabra clave a consultar");
+        return;
+    }
+
+    /* Realizar la consulta remota */
+    findLocationsNear({ keyword, radius, type });
+}
+
+/**
+ * Llamar a nuestro servidor de PHP para generar la consulta
+ * usando la latitud y longitud del centro del mapa actual
+ *
+ * @param parameters Lista de parametros para la URL, keyword, tipo etc
+ */
+function findLocationsNear(parameters) {
+    /* Borrar los resultados que ya tengamos */
+    clearLocations();
+
+    $.ajax({
+        type: "GET",
+        url: "api/places/locations",
+        data: { ...parameters }, /* Enviar los parametros completos, se asume que ya estan validados */
+
+        /**
+         * Meter los marcadores en el mapa
+         */
+		success: (response) => {
+			const bounds = new google.maps.LatLngBounds();
+
+            response.results.forEach((result, index) => {
+                const latlng = new google.maps.LatLng(result.lat, result.lng);
+
+                createOption(result.name, index);
+                createMarker(latlng, result.name, result.formatted_address);
+
+                bounds.extend(latlng);
+            });
+
+            /* Ampliar las puntas del mapa */
+            map.fitBounds(bounds);
+            showLocationSelect();
+        },
+
+        /**
+         * Ocurrio algun problema, mostramos el mensaje de error
+         */
+        error: (response) => {
+            alert(response.message);
+        }
+    });
+}
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,19 +11,31 @@
 
 <body style="margin:0px; padding:0px;" onload="initMap()">
     <div>
-        <label for="raddressInput">Search location:</label>
+        <label for="addressInput">Nombre de la ubicacion:</label>
         <input type="text" id="addressInput" size="15" />
-        <label for="radiusSelect">Radius:</label>
-        <select id="radiusSelect" label="Radius">
-            <option value="50" selected>50 kms</option>
-            <option value="30">30 kms</option>
-            <option value="20">20 kms</option>
-            <option value="10">10 kms</option>
+
+        <label for="radiusSelect">Radio:</label>
+        <select id="radiusSelect" label="Radio">
+            <option value="50" selected>50 KM</option>
+            <option value="30">30 KM</option>
+            <option value="20">20 KM</option>
+            <option value="10">10 KM</option>
         </select>
 
+        {{-- Consultar tambien el tipo de ubicacion --}}
+        <label for="typeInput">Tipo de ubicacion:</label>
+        <select id="typeInput">
+            @foreach ($types as $type)
+                <option value="{{ $type }}">{{ $type }}</option>
+            @endforeach
+        </select>
         <input type="button" id="searchButton" value="Search" />
     </div>
-    <div><select id="locationSelect" style="width: 10%; visibility: hidden"></select></div>
+    <div>
+        <select id="locationSelect" style="width: 10%; visibility: hidden"></select>
+    </div>
+
+    {{-- Visualizacion del Mapa --}}
     <div id="map" style="width: 100%; height: 90%"></div>
 
     {{-- Cargar el mapa --}}
@@ -32,7 +44,6 @@
     {{-- Metodos AJAX para guardar --}}
     <script src="js/main.js"></script>
 
-    {{-- TODO: agregar el  --}}
     <script async defer src="https://maps.googleapis.com/maps/api/js?key={{ $api_key }}&callback=initMap">
     </script>
 </body>
